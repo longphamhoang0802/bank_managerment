d762c463f332f4291c3f9776d40cabeb18c3d8cd
longphamhoang0802 <116854824+longphamhoang0802@users.noreply.github.com>
2023-07-26 10:32:36 +0700

    update database

--- src/bank/management/system/SignUp.java
+++ src/bank/management/system/SignUp.java
@@ -1,18 +1,19 @@
 package bank.management.system;
 
-//import com.toedter.calendar.JDateChooser;
+import com.toedter.calendar.JDateChooser;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.sql.PreparedStatement;
 import java.util.EventListener;
 
 public class SignUp extends JFrame implements EventListener {
     JRadioButton r1,r2,m1,m2,m3;
     JButton next;
 
-    JTextField textName ,textFname, textEmail,textAdd,textcity,textPhoneNumber;
-    //JDateChooser dateChooser;
+    JTextField textName ,textFname, textEmail,textUsername,textPassword,textPhoneNumber;
+    JDateChooser dateChooser;
     SignUp(){
         super("Sign-up");
 
@@ -53,10 +54,10 @@ public class SignUp extends JFrame implements EventListener {
         DOB.setBounds(30,260,200,30);
         add(DOB);
 
-//        dateChooser = new JDateChooser();
-//        dateChooser.setForeground(new Color(105,105,105));
-//        dateChooser.setBounds(300,340,400,30);
-//        add(dateChooser);
+        dateChooser = new JDateChooser();
+        dateChooser.setForeground(new Color(105,105,105));
+        dateChooser.setBounds(200,260,350,25);
+        add(dateChooser);
 
         JLabel labelG = new JLabel("Gender");
         labelG.setFont(new Font("Raleway", Font.BOLD, 16));
@@ -118,25 +119,25 @@ public class SignUp extends JFrame implements EventListener {
         buttonGroup1.add(m2);
         buttonGroup1.add(m3);
 
-        JLabel labelAdd = new JLabel("Address :");
+        JLabel labelAdd = new JLabel("UserName :");
         labelAdd.setFont(new Font("Raleway", Font.BOLD, 16));
         labelAdd.setBounds(30,420,200,30);
         add(labelAdd);
 
-        textAdd = new JTextField();
-        textAdd.setFont(new Font("Raleway",Font.BOLD, 14));
-        textAdd.setBounds(200,420,350,25);
-        add(textAdd);
+        textUsername = new JTextField();
+        textUsername.setFont(new Font("Raleway",Font.BOLD, 14));
+        textUsername.setBounds(200,420,350,25);
+        add(textUsername);
 
-        JLabel labelCity = new JLabel("City :");
+        JLabel labelCity = new JLabel("Password :");
         labelCity.setFont(new Font("Raleway", Font.BOLD, 16));
         labelCity.setBounds(30,460,200,30);
         add(labelCity);
 
-        textcity = new JTextField();
-        textcity.setFont(new Font("Raleway",Font.BOLD, 14));
-        textcity.setBounds(200,460,350,25);
-        add(textcity);
+        textPassword = new JTextField();
+        textPassword.setFont(new Font("Raleway",Font.BOLD, 14));
+        textPassword.setBounds(200,460,350,25);
+        add(textPassword);
 
         JLabel labelphonenumber = new JLabel("Phone Number :");
         labelphonenumber.setFont(new Font("Raleway", Font.BOLD, 16));
@@ -148,12 +149,12 @@ public class SignUp extends JFrame implements EventListener {
         textPhoneNumber.setBounds(200,500,350,25);
         add(textPhoneNumber);
 
-        next = new JButton("Next");
+        next = new JButton("Create");
         next.setFont(new Font("Raleway",Font.BOLD, 14));
         next.setBackground(Color.BLACK);
         next.setForeground(Color.WHITE);
-        next.setBounds(450,600,80,30);
-        next.addActionListener(this::setNext);
+        next.setBounds(400,600,150,30);
+        next.addActionListener(this::createUser);
         add(next);
 
         ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("icon\\backbg.png"));
@@ -168,13 +169,53 @@ public class SignUp extends JFrame implements EventListener {
         setLocation(400,40);
         setVisible(true);
     }
-    public  void setNext(ActionEvent e){
+    public  void createUser(ActionEvent e){
+        String username = textUsername.getText();
+        String password = textPassword.getText();
         String name = textName.getText();
         String fname = textFname.getText();
+        String dob = ((JTextField) dateChooser.getDateEditor().getUiComponent()).getText();
+        String gener = null;
+        if(r1.isSelected()){
+            gener = "male";
+        } else if (r2.isSelected()) {
+            gener = "female";
+        }
         String email = textEmail.getText();
-        String address = textAdd.getText();
+        String married_stt = null;
+        if(m1.isSelected()){
+            married_stt = "married";
+        } else if (m2.isSelected()) {
+            married_stt = "unmarried";
+        } else if (m3.isSelected()) {
+            married_stt = "other";
+        }
         String phone = textPhoneNumber.getText();
-        String city = textcity.getText();
-        new SignUp2();
+
+        try {
+            Connect connect1 = new Connect();
+            String q = "INSERT INTO signup (username, password, name, fname, dob, gener, email, married_stt, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connect1.connection.prepareStatement(q);
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, name);
+            preparedStatement.setString(4, fname);
+            preparedStatement.setString(5, dob);
+            preparedStatement.setString(6, gener);
+            preparedStatement.setString(7, email);
+            preparedStatement.setString(8, married_stt);
+            preparedStatement.setString(9, phone);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Thêm dữ liệu thành công.");
+            }
+            preparedStatement.close();
+            connect1.connection.close();
+            new SignUp2();
+        }catch (Exception E){
+            E.printStackTrace();
+        }
     }
 }
